888a9533a37d22d77b456b9ef80cf8d8ec5a4a40
Swarup <swarupgulla@gmail.com>
2019-12-09 17:08:28 +0000

    This is a new change for the clone

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("This is a second commit");
-        System.out.println("New Commit");
-
+        //System.out.println("This is a second commit");
+        //System.out.println("New Commit");
+        //System.out.println("added new change");
+        String str = "Hello"
+        System.out.println(str);
     }
 }
